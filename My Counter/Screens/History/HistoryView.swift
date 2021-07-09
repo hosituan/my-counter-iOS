1c9a1cd3ea79c86ab69449d511632c5908c08474
Lam Gia Khanh <lamgiakhanh.95cb1@gmail.com>
2021-07-09 19:30:12 +0700

    [HistoryScreen] Update View for Multiple counting

--- My Counter/Screens/History/HistoryView.swift
+++ My Counter/Screens/History/HistoryView.swift
@@ -13,23 +13,42 @@ import SwiftUIListSeparator
 struct HistoryView: View {
     @Environment(\.viewController) var viewControllerHolder: ViewControllerHolder
     @ObservedObject var historyViewModel = HistoryViewModel()
+    @State private var selection = Set<String>()
+    @State private var isCounting = false
+
+
     var body: some View {
-        List {
+        List(selection: $selection) {
             ForEach(historyViewModel.historyList.indices, id: \.self) { index in
                 HistoryRow(item: historyViewModel.historyList[index])
                     .onTapGesture {
-                        viewControllerHolder.value?.present(style: .fullScreen) {
-                            ZStack(alignment: .top) {
-                                PreviewViewImage(link: historyViewModel.historyList[index].url, text: "\(Strings.EN.CountResultTitle)\(historyViewModel.historyList[index].name): \(historyViewModel.historyList[index].count)")
-                                    .edgesIgnoringSafeArea(.all)
+                        if (!isCounting){
+                            viewControllerHolder.value?.present(style: .fullScreen) {
+                                ZStack(alignment: .top) {
+                                    PreviewViewImage(link: historyViewModel.historyList[index].url, text: "\(Strings.EN.CountResultTitle)\(historyViewModel.historyList[index].name): \(historyViewModel.historyList[index].count)")
+                                        .edgesIgnoringSafeArea(.all)
+                                }
                             }
+                            
+                        } else {
                             
                         }
                     }
                     .listRowBackground(Color.clear)
-            }.onDelete(perform: historyViewModel.deleteHistory(at:))
+            }
+            .onDelete(perform: historyViewModel.deleteHistory(at:))
             
+
         }
+        .environment(\.editMode, .constant(self.isCounting ? EditMode.active : EditMode.inactive)).animation(Animation.spring())
+        .toolbar(content: {
+            Button(action: {
+                self.isCounting.toggle()
+            }, label: {
+                Text(isCounting ? "Done" : "Count")
+                                    .frame(width: 80, height: 40)
+            })
+        })
         .listSeparatorStyle(.none)
         .onAppear() {
             historyViewModel.loadHistory()
